b7f3816c120eccdaed0211e5cd6770712ba1de79
samimeadad <sami.meadad@gmail.com>
2022-10-10 23:44:02 +0600

    carousel background added

--- src/Components/Home/ImageSlider.js
+++ src/Components/Home/ImageSlider.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import banner2 from '../../images/banner/2.jpg';
 import banner3 from '../../images/banner/3.jpg';
@@ -9,6 +9,7 @@ import banner6 from '../../images/banner/6.jpg';
 import banner7 from '../../images/banner/7.jpg';
 import banner8 from '../../images/banner/8.jpg';
 import banner9 from '../../images/banner/9.jpg';
+import background from '../../images/background/blur-background.jpg'
 import emailjs from '@emailjs/browser';
 
 
@@ -37,13 +38,13 @@ const ImageSlider = () => {
     };
 
     return (
-        <div className='my-5 overflow-hidden'>
-            <Row className='g-5 overflow-hidden'>
+        <div className='w-100 overflow-hidden p-5' style={ { backgroundImage: `url(${ background })`, backgroundSize: "cover", overflow: 'hidden' } }>
+            <Row className='overflow-hidden g-5'>
                 <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                     <div className='text-center ms-4 mt-5'>
-                        <h1 className='text-start text-success fw-bold fs-1 my-3'>Global Pest Control</h1>
-                        <h1 className='text-start text-secondary fw-bold fs-1 my-3'>For A Pest-Free Home</h1>
-                        <h1 className='text-start text-danger fw-bold fs-1 my-3'>Pest Termination To Perfection</h1>
+                        <h1 className='text-start text-light fw-bold fs-1 my-3'>Global Pest Control</h1>
+                        <h1 className='text-start text-warning fw-bold fs-1 my-3'>For A Pest-Free Home</h1>
+                        <h1 className='text-start text-light fw-bold fs-1 my-3'>Pest Termination To Perfection</h1>
                     </div>
                 </Col>
                 <Col xs={ 12 } sm={ 12 } md={ 6 } lg={ 6 }>
@@ -54,7 +55,6 @@ const ImageSlider = () => {
                                 src={ banner3 }
                                 alt="Second slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Second slide label</h3>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -66,7 +66,6 @@ const ImageSlider = () => {
                                 src={ banner4 }
                                 alt="Third slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Third slide label</h3>
                                 <p>
@@ -80,7 +79,6 @@ const ImageSlider = () => {
                                 src={ banner2 }
                                 alt="Fourth slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Fourth slide label</h3>
                                 <p>
@@ -94,7 +92,6 @@ const ImageSlider = () => {
                                 src={ banner5 }
                                 alt="Fifth slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Fifth slide label</h3>
                                 <p>
@@ -108,7 +105,6 @@ const ImageSlider = () => {
                                 src={ banner6 }
                                 alt="Sixth slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Sixth slide label</h3>
                                 <p>
@@ -122,7 +118,6 @@ const ImageSlider = () => {
                                 src={ banner7 }
                                 alt="Seventh slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Seventh slide label</h3>
                                 <p>
@@ -136,7 +131,6 @@ const ImageSlider = () => {
                                 src={ banner8 }
                                 alt="Eighth slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Eighth slide label</h3>
                                 <p>
@@ -150,7 +144,6 @@ const ImageSlider = () => {
                                 src={ banner9 }
                                 alt="Ninth slide"
                             />
-
                             <Carousel.Caption>
                                 <h3>Ninth slide label</h3>
                                 <p>
@@ -162,9 +155,9 @@ const ImageSlider = () => {
                 </Col>
                 <Col xs={ 12 } sm={ 12 } md={ 3 } lg={ 3 }>
                     <div className='text-center my-2 overflow-hidden'>
-                        <h2 className='fw-bold text-success mb-3'>Get A Free Quote</h2>
+                        <h2 className='fw-bold text-light mb-3'>Get A Free Quote</h2>
                         {
-                            success ? <h6 className="text-success">{ successMessage }</h6> : <h6 className="text-danger">{ error }</h6>
+                            success ? <h6 className="text-light">{ successMessage }</h6> : <h6 className="text-danger">{ error }</h6>
                         }
                         <Form onSubmit={ sendEmail } className="form w-75 mx-auto overflow-hidden">
                             <Form.Group className="mb-3" controlId="formBasicName">
@@ -175,7 +168,7 @@ const ImageSlider = () => {
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Control type="email" placeholder="Email" name='email' required />
-                                <Form.Text className="text-muted">
+                                <Form.Text className="text-light">
                                     We'll never share your email with anyone else.
                                 </Form.Text>
                             </Form.Group>
@@ -191,11 +184,11 @@ const ImageSlider = () => {
                                 </Form.Select>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label className='fw-bold'>Please Write Your Address Below</Form.Label>
+                                <Form.Label className='fw-bold text-light'>Please Write Your Address Below</Form.Label>
                                 <Form.Control as="textarea" rows={ 5 } name='message' required />
                             </Form.Group>
-                            <Button variant="success" type="submit">
-                                Get A Quote
+                            <Button variant="danger" type="submit" className='p-2'>
+                                <h4 className='fw-bold'>Get A Quote</h4>
                             </Button>
                         </Form>
                     </div>
