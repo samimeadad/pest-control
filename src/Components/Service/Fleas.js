678f2008629c918bf09ee15592611732f25be8f6
samimeadad <sami.meadad@gmail.com>
2022-10-15 21:23:59 +0600

    flea page done

--- src/Components/Service/Fleas.js
+++ src/Components/Service/Fleas.js
@@ -6,9 +6,9 @@ import fleas1 from '../../images/service/main/fleas1.jpg';
 import fleas2 from '../../images/service/main/fleas2.jpg';
 import cockroach3 from '../../images/service/main/cockroach3.jpg';
 import cockroach4 from '../../images/service/main/cockroach4.jpg';
-import cockroach5 from '../../images/service/main/cockroach5.jpg';
-import cockroach6 from '../../images/service/main/cockroach6.jpg';
-import cockroach7 from '../../images/service/main/cockroach7.jpg';
+import fleas5 from '../../images/service/main/fleas5.jpg';
+import fleas6 from '../../images/service/main/fleas6.jpg';
+import fleas7 from '../../images/service/main/fleas7.jpg';
 import { Link } from 'react-router-dom';
 import ScrollToTop from 'react-scroll-to-top';
 
@@ -33,7 +33,7 @@ const Fleas = () => {
                                 Fleas can lay between 4-8 eggs every time, producing up to 1000 eggs in their entire 2-year lifetime. These unpleasant pests can swarm a house very quickly if not treated properly. If you have spotted fleas in your home, act promptly to stop the spread.
                             </p>
                             <p className='text-start'>
-                                Eliminate Solutions are your local, friendly flea extermination experts. We use the latest in flea removal equipment that is safe for homeowners and their pets, which entirely removes the fleas from your property. We are available 24/7, all year round. We have affordable pest control treatment options for you to choose from that will banish fleas from your home. We can cover Scotland and most of Northern England.
+                                Global Pest Control are your local, friendly flea extermination experts. We use the latest in flea removal equipment that is safe for homeowners and their pets, which entirely removes the fleas from your property. We are available 24/7, all year round. We have affordable pest control treatment options for you to choose from that will banish fleas from your home. We can cover Scotland and most of Northern England.
                             </p>
                             <p className='text-start text-danger'>
                                 <Button variant='danger' className='p-3'><h4><FontAwesomeIcon className="text-light" icon={ faPhoneVolume } /> <a className="text-decoration-none text-light fw-bold" href="tel: +4407728957714"> : 077 2895 7714</a></h4></Button>
@@ -66,7 +66,7 @@ const Fleas = () => {
                     </Row>
                 </div>
                 <div className='my-5 overflow-hidden'>
-                    <h2 className='my-5 text-start'>Eradication of Cockroaches</h2>
+                    <h2 className='my-5 text-start'>Eradication of Fleas</h2>
                     <Row className='g-5'>
                         <Col xs={ 12 } sm={ 12 } md={ 5 } lg={ 5 }>
                             <img src={ cockroach3 } alt="cockroach" className='img-fluid mb-2' />
@@ -74,26 +74,26 @@ const Fleas = () => {
                         </Col>
                         <Col xs={ 12 } sm={ 12 } md={ 7 } lg={ 7 }>
                             <p className='text-start mb-5 mt-3'>
-                                Our Cockroach programmes use a knockdown spray to remove as many of these unwanted pests as possible in one go.   The spray we use not only kills the eggs, adult and juvenile (nymph) cockroaches on contact with the insect but will also remain active for some time after the initial spray treatment. In conjunction with this, we will also treat the affected area with an edible poison in a gel form, which will round up any cockroaches that the initial spray may miss.
+                                Our Flea Control programmes use a knockdown spray to remove as many of these unwanted pests as possible in one go. The spray we use not only kills the eggs, adult and juvenile (nymph) fleas on contact with the insect but will also remain active for some time after the initial spray treatment. In conjunction with this, we will also treat the affected area with an edible poison in a gel form, which will round up any cockroaches that the initial spray may miss.
                             </p>
                             <p className='text-start mb-5'>
-                                Cockroaches are very resilient pests and are tough to eradicate. Their ability to breed rapidly makes cockroach pest removal services even more vital. If you can catch the infestation quickly enough, it will be easier to manage the situation. Eliminate Solutions has only the best elimination equipment to handle any size cockroach infestation.
+                                Fleas are very resilient pests and are tough to eradicate. Their ability to breed rapidly makes flea pest removal services even more vital. If you can catch the infestation quickly enough, it will be easier to manage the situation. Global Pest Control has only the best elimination equipment to handle any size flea infestation.
                             </p>
                             <p className='text-start mb-5'>
-                                Cockroaches can damage a commercial premises’ reputation, mainly if it is related to food. In addition, cockroaches are well known to carry several diseases, which can lead to illnesses such as food poisoning, tapeworm, dysentery, fungus, roundworm and other viruses and allergies.
+                                Fleas can survive for at least two weeks without eating. They are very aggressive feeders and will feed from a person should they need to. They need to eat so they can lay their eggs, so everything that moves is lunch.
                             </p>
                             <p className='text-start mb-5'>
-                                Cockroaches also carry an unpleasant odour, which can be offputting in a food-related environment. A single cockroach might not have such a pungent smell, but an infestation might overwhelm the building. In addition, they carry a sweet, musty smell that is likened to almonds. This is usually a good telltale sign that you have cockroaches.
+                                Fleas are capable of jumping at an impressive 13 inches, which is the equivalent of a human jumping 1000 feet.
                             </p>
-                            <p className='text-start mb-5 text-danger'>
-                                <h3>Call Global Pest Control Service 07728 957 714 to learn more about our cockroach pest control services.</h3>
+                            <p className='text-center text-danger'>
+                                <Link to="/contact" className='text-decoration-none text-light'><Button variant='success' className='p-4 my-3'><h4>Get Quote</h4></Button></Link>
                             </p>
                         </Col>
                     </Row>
                 </div>
             </Container>
             <div className='w-100 overflow-hidden text-center bg-success text-light my-5'>
-                <h2 className='mt-5'>Have your cockroach problems Eliminated.</h2>
+                <h2 className='mt-5'>Have your Fleas problems remove forever</h2>
                 <h2>Call us today for a free quote and to arrange an appointment</h2>
                 <Button variant='light' className='p-3 my-5'><h4><FontAwesomeIcon className="text-danger" icon={ faPhoneVolume } /> <a className="text-decoration-none text-danger fw-bold" href="tel: +4407728957714"> : 07728957714</a></h4></Button>
             </div>
@@ -101,13 +101,13 @@ const Fleas = () => {
                 <Row className='g-5 my-5'>
                     <Col xs={ 12 } sm={ 12 } md={ 7 } lg={ 7 }>
                         <p className='text-start mb-5 mt-3'>
-                            In some situations, insect proofing will go a long way to help with issues on domestic and commercial premises. Eliminate can provide one of the eradication services as well as ongoing roach monitoring and control programmes. Your Technician will advise you of the best products available to make your property a fortress against re-infestation.
+                            Several household items will help control infestation, but using a British Pest Control Association member such as Eliminate, means your home will be treated with much more potent products than available in a shop and will kill off the flea’s lifecycle, not just the ones you can see.
                         </p>
                         <p className='text-start mb-5'>
-                            It is responsible for tackling a cockroach pest problem as quickly as possible. Otherwise, they could soon spread to neighbouring properties. Once we have health with your cockroach pest problem, then there are many steps you can take to prevent a future cockroach infestation.
+                            Bites can be painful, but that’s primarily due to the irritation the bites can cause. If not treated swiftly, fleas can have a damaging effect. Within businesses, this can affect your staff’s morale and reputation. If customers are affected, they may stay away from your business and recovery from instances like this can cause massive disruption to your business.
                         </p>
                         <p className='text-start mb-5'>
-                            Inspect your home for hiding places in warm or dark areas or near where there are food sources and water. Ensure you always clean your house of crumbs and dampness whenever possible to remove the temptation of the cockroach moving in. You can also lay a few sticky traps in suspect areas to try and catch them yourself. Try and discover entry points into your home and fill those gaps to prevent entry. Repair cracks in walls, refill the edges of windows with caulk and repair any water leaks you may have.
+                            Fleas are blood-feeding pests that reproduce quicker in warmer climates. They can survive the winter by nesting in a heated home. Each adult flea is smaller than a grain of rice and has no wings. They are extremely strong jumpers.
                         </p>
                         <p className='text-start mb-5'>
                             <h3 className='mb-3'>What do we offer?</h3>
@@ -119,14 +119,14 @@ const Fleas = () => {
                         <p className='text-start mb-5 text-danger'>
                             <h3>Call Global Pest Control on 07728 957 714 to discover more about our cockroach pest control services.</h3>
                         </p>
-                        <p className='text-start mb-5 text-danger'>
-                            <Link to="/contact" className='text-decoration-none text-light'><Button variant='success' className='p-4 my-3'>Get Quote</Button></Link>
+                        <p className='text-start text-danger'>
+                            <Link to="/contact" className='text-decoration-none text-light'><Button variant='success' className='p-4 my-3'><h4>Get Quote</h4></Button></Link>
                         </p>
                     </Col>
                     <Col xs={ 12 } sm={ 12 } md={ 5 } lg={ 5 }>
-                        <img src={ cockroach5 } alt="cockroach" className='img-fluid' />
-                        <img src={ cockroach6 } alt="cockroach" className='img-fluid my-3' />
-                        <img src={ cockroach7 } alt="cockroach" className='img-fluid' />
+                        <img src={ fleas5 } alt="fleas5" className='img-fluid' />
+                        <img src={ fleas7 } alt="cockroach" className='img-fluid my-2' />
+                        <img src={ fleas6 } alt="fleas6" className='img-fluid' />
                     </Col>
                 </Row>
             </Container>
