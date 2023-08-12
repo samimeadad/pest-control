8867fe31b516c0a14375729952337c7e9706c82d
samimeadad <sami.meadad@gmail.com>
2023-08-12 16:17:49 +0600

    navbar modification done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -14,39 +14,37 @@ const Header = () => {
                 <Container className='overflow-hidden text-center w-100'>
                     <Navbar collapseOnSelect expand="lg" bg-light variant="light" className='text-center w-100'>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" className='ms-3' />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Navbar.Brand className='d-flex align-items-center text-center'>
-                                <div className='me-3 text-center'>
-                                    <NavLink to="/" className="me-5">
-                                        <img className='img-fluid' style={ { width: '150px' } } src={ logo } alt="logoImage" />
-                                    </NavLink>
-                                </div>
-                                <div>
-                                    <h1 className='text-success fw-bold'>Global Pest Control</h1>
-                                </div>
-                            </Navbar.Brand>
-                            <Nav className="ms-auto">
-                                <NavLink className="me-4 text-success text-decoration-none btn btn-light" to="/home"><h3 className='fw-bold'>Home</h3></NavLink>
-                                <NavLink className="me-4 text-success text-decoration-none btn btn-light" to="/about"><h3 className='fw-bold'>About</h3></NavLink>
-                                <NavLink className="me-4 text-success text-decoration-none btn btn-light" to="/service"><h3 className='fw-bold'>Services</h3></NavLink>
-                                <NavLink className="me-4 text-success text-decoration-none btn btn-light" to="/contact"><h3 className='fw-bold'>Contact</h3></NavLink>
-                            </Nav>
-                        </Navbar.Collapse>
+                        <Navbar.Brand className='d-flex align-items-center text-center'>
+                            <div className='text-center'>
+                                <NavLink to="/" className="me-5">
+                                    <img className='img-fluid' style={ { width: '150px' } } src={ logo } alt="logoImage" />
+                                </NavLink>
+                            </div>
+                            <div className='text-center'>
+                                <h1 className='text-success fw-bold'>Global Pest Control</h1>
+                            </div>
+                        </Navbar.Brand>
+                        <Nav className="ms-auto pe-2">
+                            <NavLink className="text-success text-decoration-none btn btn-light" to="/service"><h2 className='fw-bold'>Our Services</h2></NavLink>
+                        </Nav>
+                        <nav className='ms-auto'>
+                            <div className='text-center p-3 overflow-hidden'>
+                                <Button variant='danger' className='p-3'><h3><FontAwesomeIcon className="text-light" icon={ faPhoneVolume } /> <a className="text-decoration-none text-light fw-bold" href="tel: +4407728957714"> : 077 2895 7714</a></h3></Button>
+                            </div>
+                        </nav>
+                        <nav className='ms-auto'>
+                            <div className='mt-3'>
+                                <p className="text-center">
+                                    <Link to={ { pathname: "https://www.facebook.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-facebook text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Facebook"></i></Link>
+                                    <Link to={ { pathname: "https://www.twitter.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-twitter text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Twitter"></i></Link>
+                                    <Link to={ { pathname: "https://www.youtube.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fab fa-youtube text-danger fw-bold me-3 fs-4" data-toggle="tooltip" title="Github"></i></Link>
+                                    <Link to={ { pathname: "https://www.linkedin.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-linkedin text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Linkedin"></i></Link>
+                                    <Link to={ { pathname: "https://www.instagram.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fab fa-instagram text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Github"></i></Link>
+                                </p>
+                            </div>
+                        </nav>
                     </Navbar>
                 </Container>
-            </div>
-            <div className='text-center p-3 overflow-hidden'>
-                <h3 className='text-danger fw-bold'>Call Now</h3>
-                <Button variant='danger' className='p-3'><h3><FontAwesomeIcon className="text-light" icon={ faPhoneVolume } /> <a className="text-decoration-none text-light fw-bold" href="tel: +4407728957714"> : 077 2895 7714</a></h3></Button>
-                <div className='mt-3'>
-                    <p className="text-center">
-                        <Link to={ { pathname: "https://www.facebook.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-facebook text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Facebook"></i></Link>
-                        <Link to={ { pathname: "https://www.twitter.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-twitter text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Twitter"></i></Link>
-                        <Link to={ { pathname: "https://www.youtube.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fab fa-youtube text-danger fw-bold me-3 fs-4" data-toggle="tooltip" title="Github"></i></Link>
-                        <Link to={ { pathname: "https://www.linkedin.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fa fa-linkedin text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Linkedin"></i></Link>
-                        <Link to={ { pathname: "https://www.instagram.com/" } } target="_blank" rel="noreferrer"><i aria-hidden="true" className="fab fa-instagram text-primary fw-bold me-3 fs-4" data-toggle="tooltip" title="Github"></i></Link>
-                    </p>
-                </div>
             </div>
             <Container className='my-5 overflow-hidden'>
                 <hr />
