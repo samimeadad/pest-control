8c973d75c6003f4f5f00891f6e98b5a225075fb9
samimeadad <sami.meadad@gmail.com>
2023-10-09 09:46:56 +0600

    BrowserRouter changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './Components/About/About';
 import AboutHeader from './Components/About/AboutHeader';
 import Contact from './Components/Contact/Contact';
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 );
 
